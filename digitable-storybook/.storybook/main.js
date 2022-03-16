67bae5d31ee888aef0bf744e73de60ce63261bb0
Uzhastin-Nikita <fosfor20@mail.ru>
2022-03-16 16:51:34 +0300

    refactor: addition typescript for storybook

--- digitable-storybook/.storybook/main.js
+++ digitable-storybook/.storybook/main.js
@@ -1,16 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+    stories: [],
+    addons: [],
+    typescript: {
+      reactDocgen: 'react-docgen-typescript',
+      reactDocgenTypescriptOptions: {
+        compilerOptions: {
+          allowSyntheticDefaultImports: false,
+          esModuleInterop: false,
+        },
+      }
+    }
+  };
