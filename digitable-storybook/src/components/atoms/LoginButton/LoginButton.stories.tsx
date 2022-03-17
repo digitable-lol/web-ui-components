3157deb63e4bc90b07cdf8e4e936c5b7e98ab875
Uzhastin-Nikita <fosfor20@mail.ru>
2022-03-17 15:41:34 +0300

    feat: setup on more component

---
+++ digitable-storybook/src/components/atoms/LoginButton/LoginButton.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { LoginButton } from "./LoginButton"
+
+export default {
+  title: 'Login Button',
+  component: LoginButton
+} as ComponentMeta<typeof LoginButton>;
+
+const Template: ComponentStory<typeof LoginButton> = (args) => <LoginButton {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  text: 'Войти'
+};
+
