588086aabd0fc0dd62ded064004998dce973a3d7
Uzhastin-Nikita <fosfor20@mail.ru>
2022-03-28 16:29:29 +0300

    fix: addition Image and Text in index.tsx for Storybook

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
 import { LoginButton } from './components/atoms/LoginButton'
+import { Image } from './components/atoms/Image'
+import { Text } from './components/atoms/Text'
 
-export default { LoginButton }
+export default { LoginButton, Image, Text }
