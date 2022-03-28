ddc447a4fe9128b40c2e467850f9c8e544bfb74f
Uzhastin-Nikita <fosfor20@mail.ru>
2022-03-28 16:39:21 +0300

    fix: relocate import place for linter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import { LoginButton } from './components/atoms/LoginButton'
 import { Image } from './components/atoms/Image'
+import { LoginButton } from './components/atoms/LoginButton'
 import { Text } from './components/atoms/Text'
 
 export default { LoginButton, Image, Text }
