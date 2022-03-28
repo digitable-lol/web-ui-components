64bcee9490b8e09d7bbc4088cf67bb2d6d4cdaf9
Uzhastin Nikita <71666299+Uzhastin-Nikita@users.noreply.github.com>
2022-03-28 21:54:50 +0300

    fix/WUC-14: Image (#34)
    * fix: addition Image and Text in index.tsx for Storybook
    
    * fix: relocate import place for linter
    
    * refactor: change the location of variables in the export

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import { Image } from './components/atoms/Image'
 import { LoginButton } from './components/atoms/LoginButton'
+import { Text } from './components/atoms/Text'
 
-export default { LoginButton }
+export default { Image, LoginButton, Text }
