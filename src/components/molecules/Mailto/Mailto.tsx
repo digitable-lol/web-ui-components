39986c14fcf74c6c17c2d09c0fe1acd33e5eaa95
Marat Zimnurov <zimtir@mail.ru>
2022-04-20 09:31:02 +0000

    feat(mailto): extend classNames

--- src/components/molecules/Mailto/Mailto.tsx
+++ src/components/molecules/Mailto/Mailto.tsx
@@ -1,24 +1,34 @@
 import React from 'react'
 
+import cx from 'classnames'
+
 import css from './Mailto.styles.module.scss'
 
 export type MailtoProps = {
   mailto: string
   label: string
   title: string
+
+  // Extend props
+  classNameMailto?: string
+  classNameTitle?: string
+  classNameLabel?: string
 }
 
-export const Mailto = ({ mailto, label, title }: MailtoProps) => {
+export const Mailto = ({ mailto, label, title, classNameLabel, classNameMailto, classNameTitle }: MailtoProps) => {
   return (
-    <div className={css.mailto}>
-      <p className={css.title}>{title}</p>
+    // TODO: Change className `mailto` to `container`
+    <div className={cx(css.mailto, classNameMailto)}>
+      {/* TODO: Add prefix for classes: mailtoTitle */}
+      <p className={cx(css.title, classNameTitle)}>{title}</p>
       <a
         href="#"
         onClick={event => {
           window.location.href = mailto
           event.preventDefault()
         }}
-        className={css.label}
+        // TODO: Add prefix for classes: mailtoLabel
+        className={cx(css.label, classNameLabel)}
       >
         {label}
       </a>
