b4d73e2c44e1aaea6909ce348d95019c38bd08f4
Uzhastin-Nikita <fosfor20@mail.ru>
2022-05-23 14:41:43 +0300

    feat: relocate Icon atom from WEB

---
+++ src/components/atoms/Icon/Icon.types.ts
@@ -0,0 +1,37 @@
+export enum Icons {
+  // social
+  telegram = 'telegram',
+  facebook = 'facebook',
+  vkontakte = 'vkontakte',
+  twitter = 'twitter',
+  github = 'github',
+
+  // common
+  gamepad = 'gamepad',
+  work = 'work',
+  house = 'house',
+  star = 'star',
+  domain = 'domain',
+  graphics = 'graphics',
+
+  // details
+  processor = 'processor',
+  motherboard = 'motherboard',
+  videocard = 'videocard',
+  ram = 'ram',
+  close = 'close',
+
+  // navLinks
+  call = 'call',
+  catalog = 'catalog',
+  delivery = 'delivery',
+  home = 'home',
+
+  // benefit
+  dollar = 'dollar',
+  guality = 'guality',
+  tool = 'tool',
+  modules = 'modules',
+  vector = 'vector',
+  envelope = 'envelope'
+}
