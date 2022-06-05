a4cff2a3bb5a5c8885fb2c9fde6ee8e992907a93
Uzhastin-Nikita <fosfor20@mail.ru>
2022-06-05 20:18:25 +0300

    feat: relocate ErrorCode component from web

---
+++ src/components/molecules/ErrorCode/ErrorCode.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import cx from 'classnames'
+import { Link } from 'react-router-dom'
+
+import { Image } from 'src/components/atoms/Image'
+
+import css from './ErrorCode.styles.module.scss'
+
+export type ErrorCodeProps = {
+  imageUrl?: string
+  leftNumber?: number
+  rightNumber?: number
+  simpleText?: string
+  highlightText?: string
+}
+
+export const ErrorCode = ({ imageUrl, leftNumber, rightNumber, simpleText, highlightText }: ErrorCodeProps) => {
+  return (
+    <div className={css.error}>
+      <span className={css.numeric}>{leftNumber}</span>
+      <Image src={imageUrl} width="568" height="568" />
+      <span className={css.numeric}>{rightNumber}</span>
+      <div className={css.box}>
+        <span className={css.text}>{simpleText}</span>
+        <span className={cx(css.text, css.highlightText)}> {highlightText}</span>
+        <Link to="/" className={css.button}>
+          На главную
+        </Link>
+      </div>
+    </div>
+  )
+}
