d11023b37b3286fd34ec996c9dda6a8cc6ef57f2
swbonjour <sweatybonjour@mail.ru>
2022-07-15 19:22:07 +0300

    refactor: image width height

--- src/components/atoms/Image/Image.tsx
+++ src/components/atoms/Image/Image.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export type ImageProps = {
   src: string
+  width?: string | number
+  height?: string | number
   alt: string
   className?: string
 }
 
-export const Image = ({ src, alt, className }: ImageProps) => <img src={src} alt={alt} className={className} />
+export const Image = ({ src, width, height, alt, className }: ImageProps) => (
+  <img src={src} alt={alt} className={className} width={width} height={height} />
+)
