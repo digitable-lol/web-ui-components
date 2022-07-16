a620cde88b6f686f2f341c4d7b657b85bf1f334e
swbonjour <sweatybonjour@mail.ru>
2022-07-16 11:59:56 +0300

    refactor: change input menu

---
+++ src/components/atoms/Input/Input.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import cx from 'classnames'
+
+import { FieldError, Path, UseFormRegister, ValidationRule } from 'react-hook-form'
+
+import { DEFAULT_LENGTH_CONFIGURATION } from './Input.utils'
+
+import css from './Input.module.scss'
+
+export type InputProps<T> = {
+  register: UseFormRegister<T>
+  required: boolean
+  label: Path<T>
+  error: FieldError | undefined
+  placeholder: string
+  type?: string
+  pattern?: ValidationRule<RegExp>
+  className?: string
+}
+
+export const Input = <T extends object>({
+  register,
+  required,
+  label,
+  error,
+  placeholder,
+  type = 'text',
+  pattern,
+  className
+}: InputProps<T>) => (
+  <div
+    className={cx(
+      css.digitable_input__box,
+      {
+        [css.digitableInputBoxInvalid]: error,
+        [css.digitableInputBoxValid]: !error
+      },
+      className
+    )}
+  >
+    <input
+      {...register(label, {
+        required,
+        pattern,
+        maxLength: { ...DEFAULT_LENGTH_CONFIGURATION }
+      })}
+      className={cx(css.digitableInput, className)}
+      name={label}
+      placeholder={placeholder}
+      type={type}
+      role={label}
+    />
+  </div>
+)
---
+++ src/components/atoms/Input/Input.utils.ts
@@ -0,0 +1,6 @@
+export const MAX_INPUT_LENGTH = 32
+
+export const DEFAULT_LENGTH_CONFIGURATION = {
+  value: MAX_INPUT_LENGTH,
+  message: `Вы превысили допустимое количество символов в строке ${MAX_INPUT_LENGTH}`
+}
